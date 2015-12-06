cc5525b84d0d5d7a20dbe4695e0682c63af9e547
Christophe Marchal <toff63@gmail.com>
2015-12-06 19:00:18 -0200

    Implemented Unit-2 Lesson 2 using scala (pattern matching is part of Scala, so nothing special). Added a AddSeries button

--- scala/unit-2/src/main/scala/com/example/actors/ChartActor.scala
+++ scala/unit-2/src/main/scala/com/example/actors/ChartActor.scala
@@ -1,26 +1,29 @@
 package com.example.actors
 
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.{ Actor, ActorLogging }
 import scalafx.collections.ObservableBuffer
 import javafx.scene.chart.XYChart
 import akka.actor.Props
 
-object ChartActor{
-  object Messages{
-    case class InitializeChart(series:Map[String,Seq[(Int, Int)]])
+object ChartActor {
+  object Messages {
+    case class InitializeChart(series: Map[String, Seq[(Int, Int)]])
+    case class AddSeries(series: (String, Seq[(Int, Int)]))
   }
-  def props(charts:ObservableBuffer[XYChart.Series[Number, Number]]):Props = Props(new ChartActor(charts))
+  def props(charts: ObservableBuffer[XYChart.Series[Number, Number]]): Props = Props(new ChartActor(charts))
 }
 
-class ChartActor(charts:ObservableBuffer[XYChart.Series[Number, Number]]) extends Actor with ActorLogging {
-  
+class ChartActor(charts: ObservableBuffer[XYChart.Series[Number, Number]]) extends Actor with ActorLogging {
+
   def receive = {
-  		case ChartActor.Messages.InitializeChart(series) => 
-  		  charts.delegate.clear()
-  		  series.foreach{ case (name, data) =>
-  		      charts.delegate.add(scalafx.scene.chart.XYChart.Series[Number, Number](
-                name,
-                ObservableBuffer(data.map { case (x, y) => scalafx.scene.chart.XYChart.Data[Number, Number](x, y) })))
-  		  }
+    case ChartActor.Messages.InitializeChart(series) =>
+      charts.delegate.clear()
+      series.foreach { case (name, data) => charts.delegate.add(xyChart(name, data)) }
+    case ChartActor.Messages.AddSeries((name, data)) => charts.delegate.add(xyChart(name, data))
+
   }
+
+  def xyChart(name: String, data: Seq[(Int, Int)]) = scalafx.scene.chart.XYChart.Series[Number, Number](
+    name,
+    ObservableBuffer(data.map { case (x, y) => scalafx.scene.chart.XYChart.Data[Number, Number](x, y) }))
 }
--- scala/unit-2/src/main/scala/com/example/scalafx/Main.scala
+++ scala/unit-2/src/main/scala/com/example/scalafx/Main.scala
@@ -9,32 +9,53 @@ import com.example.MonitoringMain
 import akka.actor.Props
 import com.example.actors.ChartActor
 import java.util.concurrent.atomic.AtomicLong
+import scalafx.scene.layout.HBox
+import scalafx.scene.control._
+import scalafx.geometry.{ Insets, Pos }
+import scalafx.scene.layout.Priority
+import scalafx.Includes._
+import scalafx.scene.layout.StackPane
 
 class Main extends JFXApp {
   private var counter: AtomicLong = new AtomicLong
 
   val chart = ObservableBuffer[javafx.scene.chart.XYChart.Series[Number, Number]]()
+  
   stage = new JFXApp.PrimaryStage {
     title.value = "Line Chart Sample"
-    height = 600
-    width = 800 
     scene = new Scene {
-      root = new LineChart(NumberAxis("Number of Month"), NumberAxis("")) {
-        title = "Stock Monitoring, 2010"
-        legendSide = Side.RIGHT
-        data = chart
+      root = new StackPane {
+        children = Seq(lineChart(chart), addSeriesButton)
+        margin = Insets(137)
       }
     }
   }
+  
+  def addSeriesButton() = new Button("Add series") {
+    onAction = handle {
+      chartActor ! ChartActor.Messages.AddSeries(ChartDataHelper.serie("FakeSeries-" + counter.incrementAndGet()))
+    }
+    alignmentInParent = Pos.BOTTOM_RIGHT
+//    hgrow = Priority.Always
+//    maxWidth = Double.MaxValue
+    padding = Insets(7)
+  }
+  
+  def lineChart(chart: ObservableBuffer[javafx.scene.chart.XYChart.Series[Number, Number]]) =
+    new LineChart(NumberAxis("Number of Month"), NumberAxis("")) {
+      title = "Stock Monitoring, 2010"
+      legendSide = Side.RIGHT
+      data = chart
+    }
 
   val chartActor = MonitoringMain.system.actorOf(ChartActor.props(chart), "charting")
   chartActor ! ChartActor.Messages.InitializeChart(ChartDataHelper("FakeSeries-" + counter.incrementAndGet()))
 }
 
 object ChartDataHelper {
-  def apply(title: String, points: Int = 100): Map[String, Seq[(Int, Int)]] = {
-    Map(title -> randomPoints(points))
-  }
+  def apply(title: String, points: Int = 100): Map[String, Seq[(Int, Int)]] = Map(title -> randomPoints(points))
+  
+  def serie(title: String, points: Int = 100): (String, Seq[(Int, Int)]) = (title -> randomPoints(points))
 
   private def randomPoints(numberOfPoint: Int = 100): Seq[(Int, Int)] = {
     val random = scala.util.Random
